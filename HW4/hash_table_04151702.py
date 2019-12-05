9e00ed7529690f4be5335ce1f5c6dc20177f49fb
Xu-Yidi <55709070+Xu-Yidi@users.noreply.github.com>
2019-12-06 02:39:46 +0800

    Add files via upload

---
+++ HW4/hash_table_04151702.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 15:47:17 2019
+
+@author: Yidi
+"""
+
+from Crypto.Hash import MD5
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+class MyHashSet:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        
+    def add(self, key):
+        hash_value = self.hash_function(key)
+        index = self.add_index(key)
+        
+        if self.data[index] is None:
+            self.data[index] = ListNode(hash_value)
+        else:
+            curNode = self.data[index]
+            while curNode.next is not None:
+                curNode = curNode.next
+            curNode.next = ListNode(hash_value)
+
+    def remove(self, key):
+        hash_value = self.hash_function(key)
+        index = self.add_index(key)
+                
+        while self.contains(key) is True:
+            if self.data[index].val == hash_value:
+                self.data[index] = self.data[index].next
+            else:
+                preNode = self.data[index]
+                curNode = self.data[index].next
+                while curNode is not None:
+                    if curNode.val == hash_value:
+                        preNode.next = curNode.next
+                        curNode = curNode.next
+                    else:
+                        curNode = curNode.next
+                        preNode = preNode.next    
+    
+    def contains(self, key):
+        hash_value = self.hash_function(key)
+        index = self.add_index(key)
+        
+        if self.data[index] is None:
+            return False
+        else:
+            curNode = self.data[index]
+            while curNode is not None:
+                if curNode.val == hash_value:
+                    return True
+                else:
+                    curNode = curNode.next
+            return False
+                   
+    def hash_function(self, key):
+        h = MD5.new()
+        h.update(key.encode("utf-8"))      
+        hash_value = h.hexdigest()
+        return hash_value                  
+       
+    def add_index(self, key):
+        hash_value = self.hash_function(key)
+        index = int(hash_value, 16) % self.capacity
+        return index
+
+#Reference:無任何參考資料
